e595ec3f71917f7088fe7499582e910a4cdb0b6c
Wei Tang <wtang222@gmail.com>
2013-08-26 11:32:28 -0500

    code re-org: mv workers from under awe-client to lib/workers

--- awe-client/awe-client.go
+++ awe-client/awe-client.go
@@ -3,32 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/MG-RAST/AWE/lib/conf"
-	. "github.com/MG-RAST/AWE/lib/core"
 	. "github.com/MG-RAST/AWE/lib/logger"
+	"github.com/MG-RAST/AWE/lib/worker"
 	"os"
-)
-
-var (
-	chanRaw       = make(chan *mediumwork) // workStealer -> dataMover
-	chanParsed    = make(chan *mediumwork) // dataMover -> worker
-	chanProcessed = make(chan *mediumwork) //worker -> deliverer
-	chanPermit    = make(chan bool)
-	self          = &Client{Id: "default-client"}
-	chankill      = make(chan bool)  //heartbeater -> worker
-	workmap       = map[string]int{} //workunit map [work_id]stage_id
-)
-
-type mediumwork struct {
-	workunit *Workunit
-	perfstat *WorkPerf
-}
-
-const (
-	ID_HEARTBEATER = 0
-	ID_WORKSTEALER = 1
-	ID_DATAMOVER   = 2
-	ID_WORKER      = 3
-	ID_DELIVERER   = 4
 )
 
 func main() {
@@ -59,14 +36,13 @@ func main() {
 		}
 	}
 
-	var err error
-	var profile *Client
-	profile, err = ComposeProfile()
+	profile, err := worker.ComposeProfile()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "fail to compose profile: %s\n", err.Error())
 		os.Exit(1)
 	}
-	self, err = RegisterWithProfile(conf.SERVER_URL, profile)
+
+	self, err := worker.RegisterWithProfile(conf.SERVER_URL, profile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "fail to register: %s\n", err.Error())
 		os.Exit(1)
@@ -85,30 +61,9 @@ func main() {
 	fmt.Printf("Client registered, name=%s, id=%s\n", self.Name, self.Id)
 	Log.Event(EVENT_CLIENT_REGISTRATION, "clientid="+self.Id)
 
-	control := make(chan int)
-	go heartBeater(control)
-	go workStealer(control)
-	go dataMover(control)
-	go processor(control)
-	go deliverer(control)
-	for {
-		who := <-control //block till someone dies and then restart it
-		switch who {
-		case ID_HEARTBEATER:
-			go heartBeater(control)
-			Log.Error("heartBeater died and restarted")
-		case ID_WORKSTEALER:
-			go workStealer(control)
-			Log.Error("workStealer died and restarted")
-		case ID_DATAMOVER:
-			go dataMover(control)
-			Log.Error("dataMover died and restarted")
-		case ID_WORKER:
-			go processor(control)
-			Log.Error("worker died and restarted")
-		case ID_DELIVERER:
-			go deliverer(control)
-			Log.Error("deliverer died and restarted")
-		}
+	if err := worker.InitWorkers(self); err == nil {
+		worker.StartWorkers()
+	} else {
+		fmt.Printf("failed to initialize and start workers:" + err.Error())
 	}
 }
---
+++ lib/worker/workermgr.go
@@ -0,0 +1,72 @@
+package worker
+
+import (
+	"errors"
+	"github.com/MG-RAST/AWE/lib/core"
+	. "github.com/MG-RAST/AWE/lib/logger"
+)
+
+var (
+	chanRaw       chan *mediumwork // workStealer -> dataMover
+	chanParsed    chan *mediumwork // dataMover -> processor
+	chanProcessed chan *mediumwork // processor -> deliverer
+	chanPermit    = make(chan bool)
+	self          *core.Client
+	chankill      chan bool      //heartbeater -> worker
+	workmap       map[string]int //workunit map [work_id]stage_id}
+)
+
+type mediumwork struct {
+	workunit *core.Workunit
+	perfstat *core.WorkPerf
+}
+
+const (
+	ID_HEARTBEATER = 0
+	ID_WORKSTEALER = 1
+	ID_DATAMOVER   = 2
+	ID_WORKER      = 3
+	ID_DELIVERER   = 4
+)
+
+func InitWorkers(client *core.Client) (err error) {
+	if client == nil {
+		return errors.New("InitWorkers(): empty client")
+	}
+	self = client
+	chanRaw = make(chan *mediumwork)       // workStealer -> dataMover
+	chanParsed = make(chan *mediumwork)    // dataMover -> processor
+	chanProcessed = make(chan *mediumwork) // processor -> deliverer
+	chankill = make(chan bool)             //heartbeater -> worker
+	workmap = map[string]int{}             //workunit map [work_id]stage_id
+	return
+}
+
+func StartWorkers() {
+	control := make(chan int)
+	go heartBeater(control)
+	go workStealer(control)
+	go dataMover(control)
+	go processor(control)
+	go deliverer(control)
+	for {
+		who := <-control //block till someone dies and then restart it
+		switch who {
+		case ID_HEARTBEATER:
+			go heartBeater(control)
+			Log.Error("heartBeater died and restarted")
+		case ID_WORKSTEALER:
+			go workStealer(control)
+			Log.Error("workStealer died and restarted")
+		case ID_DATAMOVER:
+			go dataMover(control)
+			Log.Error("dataMover died and restarted")
+		case ID_WORKER:
+			go processor(control)
+			Log.Error("worker died and restarted")
+		case ID_DELIVERER:
+			go deliverer(control)
+			Log.Error("deliverer died and restarted")
+		}
+	}
+}

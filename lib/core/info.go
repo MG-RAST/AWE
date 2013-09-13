0cef36f83ffd0939374609c4b28b02914b038bea
Wei Tang <wtang222@gmail.com>
2013-09-13 15:04:24 -0500

    add Xref field in Info struct

--- lib/core/info.go
+++ lib/core/info.go
@@ -7,6 +7,7 @@ import (
 
 type Info struct {
 	Name         string    `bson:"name" json:"name"`
+	Xref         string    `bson:"xref" json:"xref"`
 	Project      string    `bson:"project" json:"project"`
 	User         string    `bson:"user" json:"user"`
 	Pipeline     string    `bson:"pipeline" json:"pipeline"`

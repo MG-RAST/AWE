9f70f5bc67d30f3beb8f0299020b9f97f7004734
Wei Tang <wtang222@gmail.com>
2014-02-10 21:37:38 -0600

    new feature: awe-client can track maximum memory usage during workunit processing and report it in perf log

--- lib/controller/workController.go
+++ lib/controller/workController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/MG-RAST/AWE/lib/core"
 	e "github.com/MG-RAST/AWE/lib/errors"
 	"github.com/MG-RAST/AWE/lib/logger"
@@ -124,6 +125,8 @@ func (cr *WorkController) Update(id string, cx *goweb.Context) {
 	LogRequest(cx.Request)
 	// Gather query params
 	query := &Query{Li: cx.Request.URL.Query()}
+
+	fmt.Printf("query=%#v\n", query)
 
 	if query.Has("status") && query.Has("client") { //notify execution result: "done" or "fail"
 		notice := core.Notice{WorkId: id, Status: query.Value("status"), ClientId: query.Value("client"), Notes: ""}

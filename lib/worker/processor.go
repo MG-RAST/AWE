83b85a8ccd85840257c08b3702e05a96d75eac99
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-03-31 10:23:07 -0500

    fixed conflicts

--- lib/worker/processor.go
+++ lib/worker/processor.go
@@ -8,7 +8,7 @@ import (
 	"github.com/MG-RAST/AWE/lib/core"
 	"github.com/MG-RAST/AWE/lib/logger"
 	"github.com/MG-RAST/AWE/lib/logger/event"
-	"github.com/fsouza/go-dockerclient"
+	"github.com/wgerlach/go-dockerclient"
 	"github.com/davecgh/go-spew/spew"
 	"io"
 	"os"
@@ -19,6 +19,7 @@ import (
 	"strings"
 	"bytes"
 	"time"
+	"compress/gzip"
 )
 
 func processor(control chan int) {
@@ -106,7 +107,7 @@ func RunWorkunitDocker(work *core.Workunit) (pstats *core.WorkPerf, err error) {
 	container_name := "AWE_container"
 		
 	Dockerimage := work.Cmd.Dockerimage
-		
+	
 	logger.Debug(1, fmt.Sprintf("Dockerimage: %s", Dockerimage))		
 	
 
@@ -170,86 +171,47 @@ func RunWorkunitDocker(work *core.Workunit) (pstats *core.WorkPerf, err error) {
 	
 	if err != nil {
 		fmt.Println("image not found !? ")
-		logger.Debug(1, fmt.Sprintf("docker image %s is not in local repository", Dockerimage))
+
+		logger.Debug(1, fmt.Sprintf("docker image %s is not yet in local repository", Dockerimage))
 		
-		//"docker=1&docker_image_name=wgerlach/bowtie2:2.2.0"
-		shock_docker_repo := core.ShockClient{conf.SHOCK_DOCKER_IMAGE_REPOSITORY, ""}
 		
-		query_response_p, err := shock_docker_repo.Query(url.Values{"docker": {"1"}, "docker_image_name" : {"wgerlach/bowtie2:2.2.0"}});
+		
+		image_retrieval := "load"
+		switch  {
+			case image_retrieval == "load" : { // for images that have been saved
+				err = dockerLoadImage(client, Dockerimage)
+			}
+			case image_retrieval == "import" : { // for containeres that have been exported
+				err = dockerImportImage(client, Dockerimage)
+			}
+			case image_retrieval == "build" : { // to create image from Dockerfile
+				err = dockerBuildImage(client, Dockerimage)	
+			}
+		}
+		
 		if err != nil {
-			return nil, errors.New(fmt.Sprintf("shock node not found, err=%s", err.Error()))
-		}
-		logger.Debug(1, fmt.Sprintf("query result: %v", query_response_p))
-		
-		datalen := len((*query_response_p).Data)
-		
-		if datalen == 0 {
-			return nil, errors.New(fmt.Sprintf("no image found in shocks docker repo"))
-		} else if datalen > 1 {
-			return nil, errors.New(fmt.Sprintf("more than one image found in shocks docker repo"))
-		}
-		
-		node := (*query_response_p).Data[0]
-		logger.Debug(1, fmt.Sprintf("found SHOCK node for docker image: %s", node.Id))
-		
-		// *** download image
-		docker_image_tarfile := path.Join(conf.DATA_PATH, "dockerimage.tar")
-		
-		if _, err := os.Stat(docker_image_tarfile); err == nil {
- 				// file exists
-  				os.Remove(docker_image_tarfile)
-		}
-		
-		download_url, err := shock_docker_repo.Get_node_download_url(node)
-		if err != nil {
-			return nil, errors.New(fmt.Sprintf("Could not create download url, err=%s", err.Error()))
-		}
-		
-		
-		//size, err := fetchFile(docker_image_tarfile, download_url, "")
-		//if err != nil {
-		//	return nil, errors.New(fmt.Sprintf("Could not download image, url=%s, err=%s", download_url, err.Error()))
-		//}
-		
-		logger.Debug(1, fmt.Sprintf("docker image url=%s", download_url))
-		
-		// TODO import base image if needed
-		
-		// *** import image
-		
-		Dockerimage_array := strings.Split(Dockerimage, ":")
-		Dockerimage_repo, Dockerimage_tag := Dockerimage_array[0], Dockerimage_array[1]
-		
-		var buf bytes.Buffer
-		opts := docker.ImportImageOptions{
-			Source: download_url,
-			Repository: Dockerimage_repo,
-			Tag: Dockerimage_tag,
-			OutputStream: &buf,
-		}
-		
-		err = client.ImportImage(opts)
-		if err != nil {
-			return nil, errors.New(fmt.Sprintf("Error importing docker image, err=%s", err.Error()))
-		}
-					
+			return nil, errors.New(fmt.Sprintf("Docker image was not correctly imported or built, err=%s", err.Error()))
+		}					
 		// example urls							
-		// find image : http://shock.metagenomics.anl.gov/node/?query&docker=1&docker_image_name=wgerlach/bowtie2:2.2.0
+		// find image : http://shock.metagenomics.anl.gov/node/?query&docker=1&tag=wgerlach/bowtie2:2.2.0
 		// view node: http://shock.metagenomics.anl.gov/node/ed0a6b20-c535-40d7-92e8-754bb8b6b48f
 		// download http://shock.metagenomics.anl.gov/node/ed0a6b20-c535-40d7-92e8-754bb8b6b48f?download
 		
 		// last test
 		image, err = client.InspectImage(Dockerimage)
 		if err != nil {
-			return nil, errors.New(fmt.Sprintf("Docker image was not correctly imported, err=%s", err.Error()))
+			return nil, errors.New(fmt.Sprintf("(InspectImage) Docker image was not correctly imported or built, err=%s", err.Error()))
 		}
-	} 
+	} else {
+		logger.Debug(1, fmt.Sprintf("docker image %s is already in local repository", Dockerimage))
+	}
 
-
+	
 
 	imageid := image.ID
 	
-	pipe_output := fmt.Sprintf("; sleep 10 2> %s 1> %s", conf.STDERR_FILENAME ,conf.STDOUT_FILENAME)
+	pipe_output := fmt.Sprintf(" 2> %s 1> %s", conf.STDERR_FILENAME ,conf.STDOUT_FILENAME)
+
 	bash_command := fmt.Sprint(commandName, " ", strings.Join(args, " ") , " ", pipe_output)
 	
 	logger.Debug(1, fmt.Sprint("bash_command: ", bash_command))
@@ -479,6 +441,153 @@ func RunWorkunitDirect(work *core.Workunit) (pstats *core.WorkPerf, err error) {
 	logger.Event(event.WORK_END, "workid="+work.Id)
 	pstats.MaxMemUsage = MaxMem
 	return
+}
+
+
+func dockerBuildImage(client *docker.Client, Dockerimage string) (err error) {
+	
+	shock_docker_repo := core.ShockClient{conf.SHOCK_DOCKER_IMAGE_REPOSITORY, ""}
+	
+	logger.Debug(1, fmt.Sprint("try to build docker image from dockerfile, Dockerimage=", Dockerimage))
+	
+	query_response_p, err := shock_docker_repo.Query(url.Values{"dockerfile": {"1"}, "tag" : {Dockerimage}});
+	if err != nil {
+		return errors.New(fmt.Sprintf("shock node not found for dockerfile=%s, err=%s", Dockerimage, err.Error()))
+	}
+	logger.Debug(1, fmt.Sprintf("query result: %v", query_response_p))
+	
+	
+	
+	datalen := len((*query_response_p).Data)
+	
+	if datalen == 0 {
+		return errors.New(fmt.Sprintf("Dockerfile %s not found in shocks docker repo", Dockerimage))
+	} else if datalen > 1 {
+		return errors.New(fmt.Sprintf("more than one Dockerfile %s found in shocks docker repo", Dockerimage))
+	}
+	
+	node := (*query_response_p).Data[0]
+	logger.Debug(1, fmt.Sprintf("found SHOCK node for Dockerfile: %s", node.Id))
+	
+	download_url, err := shock_docker_repo.Get_node_download_url(node)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Could not create download url, err=%s", err.Error()))
+	}
+	
+	// get and build Dockerfile
+	var buf bytes.Buffer
+	opts := docker.BuildImageOptions{
+		Name: "testImage",
+		Remote: download_url,
+		SuppressOutput: true,
+		OutputStream: &buf,
+	}
+	err = client.BuildImage(opts)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error importing docker image, err=%s", err.Error()))
+	}
+	
+	return nil
+}
+
+
+func getDockerImageUrl(Dockerimage string) (download_url string, err error) {
+	
+	shock_docker_repo := core.ShockClient{conf.SHOCK_DOCKER_IMAGE_REPOSITORY, ""}
+	
+	logger.Debug(1, fmt.Sprint("try to import docker image, Dockerimage=", Dockerimage))
+	//query url = "docker=1&tag=wgerlach/bowtie2:2.2.0"
+	
+	
+	query_response_p, err := shock_docker_repo.Query(url.Values{"docker": {"1"}, "tag" : {Dockerimage}});
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("shock node not found for image=%s, err=%s", Dockerimage, err.Error()))
+	}
+	logger.Debug(1, fmt.Sprintf("query result: %v", query_response_p))
+	
+	datalen := len((*query_response_p).Data)
+	
+	if datalen == 0 {
+		return "", errors.New(fmt.Sprintf("image %s not found in shocks docker repo", Dockerimage))
+	} else if datalen > 1 {
+		return "", errors.New(fmt.Sprintf("more than one image %s found in shocks docker repo", Dockerimage))
+	}
+	
+	node := (*query_response_p).Data[0]
+	logger.Debug(1, fmt.Sprintf("found SHOCK node for docker image: %s", node.Id))
+	
+	download_url, err = shock_docker_repo.Get_node_download_url(node)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Could not create download url, err=%s", err.Error()))
+	}
+	
+	return
+}
+
+func dockerLoadImage(client *docker.Client, Dockerimage string) (err error) {
+	
+	download_url, err := getDockerImageUrl(Dockerimage)
+	if (err != nil) {
+		return errors.New(fmt.Sprintf("Error getting docker url, err=%s", err.Error()))
+	}
+	
+	image_stream, err := fetchShockStream(download_url, "") // token empty here, assume that images are public
+	if (err != nil) {
+		return errors.New(fmt.Sprintf("Error getting Shock stream, err=%s", err.Error()))
+	}
+	
+	gr, err := gzip.NewReader(image_stream) //(*Reader, error)
+	
+	
+	//http://stackoverflow.com/questions/16946978/how-to-unzip-io-readcloser
+	
+	
+	logger.Debug(1, fmt.Sprintf("loading image..."))
+	//go io.Copy(image_writer, image_stream)
+	
+	var buf bytes.Buffer
+	
+	err = client.LoadImage(gr, &buf) // in io.Reader, w io.Writer
+	if (err != nil) {
+		return errors.New(fmt.Sprintf("Error loading image, err=%s", err.Error()))
+	}
+	
+	logger.Debug(1, fmt.Sprintf("load image returned: %v", &buf))
+	return nil
+}
+
+func dockerImportImage(client *docker.Client, Dockerimage string) (err error) {
+	
+	
+	download_url, err := getDockerImageUrl(Dockerimage)
+	
+	if (err != nil) {
+		return err
+	}
+	
+	logger.Debug(1, fmt.Sprintf("docker image url=%s", download_url))
+	
+	// TODO import base image if needed
+	
+	
+	// *** import image
+	Dockerimage_array := strings.Split(Dockerimage, ":")
+	Dockerimage_repo, Dockerimage_tag := Dockerimage_array[0], Dockerimage_array[1]
+	
+	var buf bytes.Buffer
+	opts := docker.ImportImageOptions{
+		Source: download_url,
+		Repository: Dockerimage_repo,
+		Tag: Dockerimage_tag,
+		OutputStream: &buf,
+	}
+	
+	err = client.ImportImage(opts)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error importing docker image, err=%s", err.Error()))
+	}
+	
+	return	
 }
 
 func SetEnv(work *core.Workunit) (envkeys []string) {

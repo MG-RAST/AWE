bdae03b962a96e52d5b365824989bd093a4088c2
Jared bischof <jbischof@gmail.com>
2014-09-12 12:58:18 -0500

    Better error handling for clients that disappear before request to checkout workunit has completed.

--- lib/core/cqmgr.go
+++ lib/core/cqmgr.go
@@ -514,32 +514,19 @@ func (qm *CQMgr) popWorks(req CoReq) (works []*Workunit, err error) {
 }
 
 func (qm *CQMgr) filterWorkByClient(clientid string) (ids []string) {
-	client := qm.clientMap[clientid]
+	client, ok := qm.clientMap[clientid]
+	if !ok {
+		err_msg := fmt.Sprintf("error: unregistered client %s trying to checkout workunit, most likely cause is client disappeared after request to checkout workunit combined with slow response to workunit checkout request", clientid)
+		fmt.Fprintln(os.Stderr, err_msg)
+		logger.Error(err_msg)
+		return
+	}
 	for id, _ := range qm.workQueue.wait {
 		if _, ok := qm.workQueue.workMap[id]; !ok {
 			logger.Error(fmt.Sprintf("error: workunit %s is in wait queue but not in workMap", id))
 			continue
 		}
 		work := qm.workQueue.workMap[id]
-
-		// In case of edge case where pointer to workunit is in queue but workunit has been deleted
-		// If work.Info is nil, this will cause errors in execution
-		// These will be deleted by servermgr.updateQueue()
-		if work == nil || work.Info == nil {
-			continue
-		}
-
-		if client == nil {
-			fmt.Fprintf(os.Stderr, "error: Skip_work for client %s is nil", clientid)
-			logger.Error(fmt.Sprintf("error: client %s is nil", clientid))
-			continue
-		}
-
-		if client.Skip_work == nil {
-			fmt.Fprintf(os.Stderr, "error: Skip_work for client %s is nil", clientid)
-			logger.Error(fmt.Sprintf("error: Skip_work for client %s is nil", clientid))
-			continue
-		}
 
 		//skip works that are in the client's skip-list
 		if contains(client.Skip_work, work.Id) {

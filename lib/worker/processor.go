d2c1f79e18c29881070314bcc9e4b35b2d104d75
Travis Harrison <guumby@gmail.com>
2015-03-17 10:30:13 -0500

    go fmt changes

--- lib/worker/processor.go
+++ lib/worker/processor.go
@@ -64,7 +64,7 @@ func processor(control chan int) {
 		var envkeys []string
 		_ = envkeys
 
-		if (work.Cmd == nil || work.Cmd.Dockerimage == "") && (work.App !=nil && work.App.Name != "") {
+		if (work.Cmd == nil || work.Cmd.Dockerimage == "") && (work.App != nil && work.App.Name != "") {
 			envkeys, err = SetEnv(work)
 			if err != nil {
 				logger.Error("SetEnv(): workid=" + work.Id + ", " + err.Error())
@@ -99,8 +99,7 @@ func processor(control chan int) {
 		processed.perfstat.Runtime = computetime
 		processed.workunit.ComputeTime = int(computetime)
 
-
-		if (work.Cmd == nil || work.Cmd.Dockerimage == "") && (work.App !=nil && work.App.Name != "") {
+		if (work.Cmd == nil || work.Cmd.Dockerimage == "") && (work.App != nil && work.App.Name != "") {
 			if len(envkeys) > 0 {
 				UnSetEnv(envkeys)
 			}

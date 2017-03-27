582a06f5031de3153699b45fab68a7c69c975d01
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2017-03-27 11:13:58 -0500

    v0.9.33

--- lib/conf/conf.go
+++ lib/conf/conf.go
@@ -14,7 +14,7 @@ import (
 	"github.com/MG-RAST/golib/goconfig/config"
 )
 
-const VERSION string = "0.9.32"
+const VERSION string = "0.9.33"
 
 var GIT_COMMIT_HASH string // use -ldflags "-X github.com/MG-RAST/AWE/lib/conf.GIT_COMMIT_HASH <value>"
 const BasePriority int = 1

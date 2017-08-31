5f3162cce1ae93d7d27f82fc22300aba403c289c
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2017-08-31 09:50:27 -0500

    v0.9.52testing

--- lib/conf/conf.go
+++ lib/conf/conf.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-const VERSION string = "0.9.51"
+const VERSION string = "0.9.52testing"
 
 var GIT_COMMIT_HASH string // use -ldflags "-X github.com/MG-RAST/AWE/lib/conf.GIT_COMMIT_HASH <value>"
 const BasePriority int = 1

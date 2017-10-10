ca5950361ad33b908211ce9b4f5ce792a1a5ff45
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2017-10-10 14:31:47 -0500

    outsourced workflowInstance

---
+++ lib/core/workflowInstance.go
@@ -0,0 +1,95 @@
+package core
+
+import (
+	"fmt"
+	"github.com/MG-RAST/AWE/lib/core/cwl"
+	"github.com/davecgh/go-spew/spew"
+	"reflect"
+)
+
+type WorkflowInstance struct {
+	Id      string           `bson:"id" json:"id"`
+	Inputs  cwl.Job_document `bson:"inputs" json:"inputs"`
+	Outputs cwl.Job_document `bson:"outputs" json:"outputs"`
+}
+
+func NewWorkflowInstanceFromInterface(original interface{}) (wi WorkflowInstance, err error) {
+	original, err = cwl.MakeStringMap(original)
+	if err != nil {
+		return
+	}
+
+	switch original.(type) {
+	case map[string]interface{}:
+
+		original_map, ok := original.(map[string]interface{})
+		if !ok {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) not a map: %s", spew.Sdump(original))
+			return
+		}
+
+		wi = WorkflowInstance{}
+
+		id_if, has_id := original_map["id"]
+		if !has_id {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) is is missing")
+			return
+		}
+		var id_str string
+		id_str, ok = id_if.(string)
+		if !ok {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) id is not string")
+			return
+		}
+		if id_str == "" {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) id string is empty")
+			return
+		}
+
+		wi.Id = id_str
+
+		inputs_if, has_inputs := original_map["inputs"]
+		if !has_inputs {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) inputs missing")
+			return
+		}
+
+		var inputs *cwl.Job_document
+		inputs, err = cwl.NewJob_documentFromNamedTypes(inputs_if)
+		if err != nil {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) (for inputs) NewJob_document returned: %s", err.Error())
+			return
+		}
+
+		wi.Inputs = *inputs
+
+		outputs_if, has_outputs := original_map["outputs"]
+		if has_outputs {
+			var outputs *cwl.Job_document
+			outputs, err = cwl.NewJob_documentFromNamedTypes(outputs_if)
+			if err != nil {
+				err = fmt.Errorf("(NewWorkflowInstanceFromInterface) (for outputs) NewJob_document returned: %s", err.Error())
+				return
+			}
+
+			wi.Outputs = *outputs
+
+		}
+
+	case *WorkflowInstance:
+
+		wi_ptr, ok := original.(*WorkflowInstance)
+		if !ok {
+			err = fmt.Errorf("(NewWorkflowInstanceFromInterface) type assertion problem")
+			return
+		}
+
+		wi = *wi_ptr
+
+	default:
+		err = fmt.Errorf("(NewWorkflowInstanceFromInterface) type unknown, %s", reflect.TypeOf(original))
+		return
+	}
+	return
+
+}

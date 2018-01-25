3080ae51794fd99a638de42a5549022d202fc40e
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-01-25 11:19:09 -0600

    add comment

--- compile.sh
+++ compile.sh
@@ -11,3 +11,5 @@ CGO_ENABLED=0 go install -a -installsuffix cgo -v -ldflags="-X github.com/MG-RAS
 
 # use this for race debug flag
 #go install -a -v -race -ldflags "-X github.com/MG-RAST/AWE/lib/conf.GIT_COMMIT_HASH=${GITHASH}" ./awe-worker/ ./awe-server/
+
+echo "recompile with: CGO_ENABLED=0 go install ./awe-server"

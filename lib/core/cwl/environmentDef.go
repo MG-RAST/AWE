3117dd265d829bc15a303d1c03ab0636d5cc47c3
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-04-02 17:03:01 -0500

    fix parsing of environmentDef

---
+++ lib/core/cwl/environmentDef.go
@@ -0,0 +1,45 @@
+package cwl
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type EnvironmentDef struct {
+	EnvName  string     `yaml:"envName,omitempty" bson:"envName,omitempty" json:"envName,omitempty" mapstructure:"envName,omitempty"`
+	EnvValue Expression `yaml:"envValue,omitempty" bson:"envValue,omitempty" json:"envValue,omitempty" mapstructure:"envValue,omitempty"`
+}
+
+func NewEnvironmentDefFromInterface(original interface{}) (enfDev EnvironmentDef, err error) {
+
+	err = mapstructure.Decode(original, &enfDev)
+	return
+}
+
+func GetEnfDefArray(original interface{}) (array []EnvironmentDef, err error) {
+
+	array = []EnvironmentDef{}
+
+	switch original.(type) {
+	case []interface{}:
+		original_array := original.([]interface{})
+		for i, _ := range original_array {
+			var enfDev EnvironmentDef
+			enfDev, err = NewEnvironmentDefFromInterface(original_array[i])
+			if err != nil {
+				err = fmt.Errorf("(GetEnfDefArray) NewEnvironmentDefFromInterface returned: %s", err.Error())
+				return
+			}
+
+			array = append(array, enfDev)
+
+		}
+		return
+	}
+
+	err = fmt.Errorf("(GetEnfDefArray) type %s not supported", reflect.TypeOf(original))
+
+	return
+}
